a30d38763b9634bceda38010f345b08ab3b46bc3
lamnghi29909 <lelamnghi29909@gmail.com>
2022-12-17 10:34:29 +0700

    test

--- login2.js
+++ login2.js
@@ -1,22 +1,11 @@
 function abc() {
-
-    var _name = document.login.name.value;
     var _pass = document.login.password.value;
     var _gmail = document.login.gmail.value;
-    var _repass = document.login.matkhau.value
 
     var ok =  true;
 
     var ktten = /^[\p{L}'][ \p{L}'-]*[\p{L}]$/u;
     var ktgmail= /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
-
-if (!_name.match(ktten)) {
-    document.getElementById("loiName").style.display = "block";
-    ok =false;
-} else {
-    document.getElementById("loiName").style.display = "none";
-}
-console.log(_name);
 
 
 if (!_gmail.match(ktgmail)) {
@@ -32,13 +21,6 @@ if (_pass == "" ) {
     ok =false;
 } else document.getElementById("loipass").style.display = "none";
 console.log(_pass);
-
-
-if (_repass == "" ) {
-    document.getElementById("loimatkhau").style.display = "block";
-    ok =false;
-} else document.getElementById("loimatkhau").style.display = "none";
-console.log(_repass);
 
 
 
