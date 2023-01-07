6c335ca05e1a8d9472972ef695a28c4240665938
lamnghi29909 <lelamnghi29909@gmail.com>
2023-01-07 20:10:01 +0700

    test

--- test.js
+++ test.js
@@ -1,35 +1,41 @@
 const array = [
     {
+        id : 1,
         img:"img/burger.png",
         comment : "Big and Juicy Wagyu Beef Cheeseburger",
         img1 : "img/30minutes.png",
         img2 : "img/snack.png",
     },    
     {
+        id : 2,
         img :"img/fish.png",
         comment : "Fresh Lime Roasted Salmon",
         img1 : "img/30minutes.png",
         img2 : "img/fish(2).png",
     },    
     {
+        id : 3,
         img :"img/chicken.png",
         comment : "Fresh and Healthy Mixed Mayonnaise Salad",
         img1 : "img/30minutes.png",
         img2 : "img/snack.png",
     },    
     {
+        id : 4,
         img :"img/mixed.png",
         comment : "Fresh and Healthy Mixed Mayonnaise Salad",
         img1 : "img/30minutes.png",
         img2 : "img/healthy.png",
     },    
     {
+        id : 5,
         img :"img/pasta.png",
         comment : "Fresh and Healthy Mixed Mayonnaise Salad",
         img1 : "img/30minutes.png",
         img2 : "img/noodles.png",
     },    
     {
+        id : 6,
         img :"img/pancake.png",
         comment : "Fresh and Healthy Mixed Mayonnaise Salad",
         img1 : "img/30minutes.png",
@@ -42,7 +48,7 @@ function showProduct(data) {
     if (Product_item) {
         for (let item of data) {
             Product_item.innerHTML += `
-            <a href="test2.html">
+            <a href="test2.html?id=${item.id}">
                 <div class="img">
                     <img src="${item.img}" alt="">
                 </div>
@@ -61,3 +67,59 @@ function showProduct(data) {
     }
 }
 showProduct(array);
+function mainProduct() {
+    let url = new URLSearchParams(window.location.search).get("id");
+    let newarrayproduct = array.find(function(item) {
+        return item.id == url;
+    });
+    let gioithieu = document.querySelector(".gioithieu");
+    gioithieu.innerHTML = `
+    <div class="comment">
+            <h1>${newarrayproduct.comment}</h1>
+            <div class="cost">
+                <p>$30</p>
+            </div>
+            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+                Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. 
+                Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+            <input type="text" placeholder="Quanity">
+            <button>Add To Chart</button>
+    </div>
+        <div class="picture">
+            <img src="${newarrayproduct.img}" alt="">
+        </div>
+    `
+}
+mainProduct();
+let arrayCart = [];
+let arrayCartNew = JSON.parse(localStorage.getItem("cart"))
+if ( arrayCartNew == null) {
+    localStorage.setItem("cart" , JSON.stringify(arrayCart))
+}
+function addcart() {
+    let url = new URLSearchParams(window.location.search).get("id");
+    let cart = array.find(function(item) {
+        return item.id == url;
+    })
+    arrayCartNew.push({
+        img : cart.img,
+        name : cart.comment,
+    });
+    localStorage.setItem("cart" , JSON.stringify(arrayCartNew));
+}
+
+function showCart() {
+    let id = JSON.parse(localStorage.getItem("cart"));
+    console.log(id);
+    let show = document.querySelector(".special")
+    for ( let item of id) {
+        show.innerHTML += `
+        <th class="checkbox"><input type="checkbox"></th>
+        <th class="m"><img src="${item.img}" alt="" class="img"> <p class="n"> ${item.name}</p></th>
+        <th>335.000đ</th>
+        <th class="special1"><p  class="soluong">1</p></th>
+        <th>335.000đ</th>
+        <th><p class="x">x</p></th>
+        `;
+    }
+}
--- test2.js
+++ test2.js
@@ -1,15 +1,18 @@
 const array = [
     {
+        id : 1,
         img:"img/mixed.png",
         comment : "Fresh and Healthy Mixed Mayonnaise Salad",
         cost : "$30"
     },    
     {
+        id:2,
         img :"img/pasta.png",
         comment : "The Creamiest Creamy Chicken",
         cost : "$30"
     },    
     {
+        id:3,
         img :"img/pancake.png",
         comment : "Fruity Pancake with Orange & Blueberry",
         cost : "$30"
@@ -21,18 +24,42 @@ function showProduct(data) {
     if (Product_item) {
         for (let item of data) {
             Product_item.innerHTML += `
-            <div class="dishes">
+                <div class="dishes">
                 <div class="img">
                     <img src="${item.img}" alt="">
                 </div>
                 <div class="comment">
                     <p>${item.comment}</p>
                     <p class="cost">${item.cost}</p>
-                    <button>Add To Cart</button>
+                    <button>Add to Cart</button>
                 </div>
             </div>
             `;
         }
     }
 }
+
 showProduct(array);
+function mainProduct() {
+    let url = new URLSearchParams(window.location.search).get("id");
+    let newarrayproduct = array.find(function(item) {
+        return item.id == url;
+    });
+    let gioithieu = document.querySelector(".gioithieu");
+    gioithieu.innerHTML = `
+    <div class="comment">
+            <h1>${newarrayproduct.comment}</h1>
+            <div class="cost">
+                <p>$30</p>
+            </div>
+            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+                Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. 
+                Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+            <input type="text" placeholder="Quanity">
+            <button>Add To Chart</button>
+    </div>
+        <div class="picture">
+            <img src="${newarrayproduct.img}" alt="">
+        </div>
+    `
+}
